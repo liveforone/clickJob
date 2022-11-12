264998ccd2f81c4305ee3b9e6bc00a3d8328feb3
liveforone <yc4852@gmail.com>
2022-11-12 14:36:45 +0900

    Initial commit

---
+++ src/test/java/clickJob/clickJob/ClickJobApplicationTests.java
@@ -0,0 +1,13 @@
+package clickJob.clickJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
